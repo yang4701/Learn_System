e501cdc94d3440d1c8d1903e26eedd8684090e55
tedu <yang@tedu.cn>
2019-03-16 11:45:24 +0800

    .

---
+++ 框架.py
@@ -0,0 +1,34 @@
+ 
+#用户信息表
+def account_profile:
+    pass
+#用户填写信息表
+def account_userinfo:
+    pass
+#授权组表
+def auth_group:
+    pass
+#授权组权限表
+def auth_group_permissions:
+    pass
+#授权用户表
+def auth_user:
+    pass
+#机构账户表
+def business_businessaccountinfo:
+    pass
+#题库信息表
+def competition_bankinfo:
+    pass
+#选择题表
+def competition_choiceinfo:
+    pass
+#比赛信息表
+def competition_competition:
+    pass
+#答题记录表
+def competition_competitionqainfo:
+    pass
+
+
+
